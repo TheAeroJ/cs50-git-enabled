b8d96ac46b79fac4eb45bb884e4cbd6d1009de16
TheAeroJ <77020383+TheAeroJ@users.noreply.github.com>
2024-06-23 14:08:05 -0500

    Added a checker to ensure that the current rank is NULL before assigning it

--- tideman2/tideman2.c
+++ tideman2/tideman2.c
@@ -103,7 +103,10 @@ bool vote(int rank, string name, int ranks[])
     //Create a placeholder int to hold the candidate ID of our validated known-good candidate.
     int candidate_id;
     // Check whether ranks[rank + 1] is empty
-    if rans[rank + 1]
+    if rank[rank] != NULL
+    {
+        return false;
+    }
     // Check whether rank > candidate_count
     if (rank > candidate_count)
     {
